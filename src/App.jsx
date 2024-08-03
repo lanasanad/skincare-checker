369d1610337e96f696b3d8c64727cefdf82d5a87
Lana Sanad <lana.sanad@gmail.com>
2024-08-03 21:00:32 +1200

    POST method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  return (
-    <div>
-      <h1>Skincare Checker</h1>
-    </div>
-  )
+  const [ingredients, setIngredients] = useState("");
+  const [skinConcerns, setSkinConcerns] = useState("");
+  const [response, setResponse] = useState("");
+
+  async function callBackendAPI() {
+    console.log("Calling the backend API");
+
+    const apiResponse = await fetch("http://localhost:5000/api/analyze", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ingredients, skinConcerns })
+    });
+
+    const data = await apiResponse.json();
+    setResponse(data.response);
+  }
+
 }
 
-export default App
+export default App;
