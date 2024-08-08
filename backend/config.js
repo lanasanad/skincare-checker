15eb191cb7304bffefae9f7bc65fa064e2d94734
Lana Sanad <lana.sanad@gmail.com>
2024-08-08 14:06:53 +1200

    background

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+//export const API_KEY = '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';
+module.exports = {
+    API_KEY: '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'
+  };
+
