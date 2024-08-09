3374c211a2753380760ab61f5de6126a61ad9703
Lana Sanad <lana.sanad@gmail.com>
2024-08-09 19:43:05 +1200

    layout changed

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    API_KEY: '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'
-  };
-
